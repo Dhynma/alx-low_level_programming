63f83169ac6c930ba3ed19e22583eb129f5c35c0
Dhynma <chidinmaekeigwe@gmail.com>
2023-12-23 14:20:07 -0800

    task1

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,18 +13,17 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *hash_table_created;
 	unsigned long int i;
 
-	/* Allocate space for the hash table */
+	/* allocate space for the hash table */
 	hash_table_created = malloc(sizeof(hash_table_t));
+
 	if (hash_table_created == NULL)
 		return (NULL);
 
 	hash_table_created->size = size;
 	hash_table_created->array = malloc(sizeof(hash_node_t *) * size);
+
 	if (hash_table_created->array == NULL)
-	{
-		free(hash_table_created);
 		return (NULL);
-	}
 
 	for (i = 0; i < size; i++)
 	{
